25e05a1358fadcfedf4e70a36d36d10a5995ad6b
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-16 13:11:27 -0300

    quase-lá

---
+++ script.js
@@ -0,0 +1,8 @@
+function menuShow(){
+    let menumobile = document.querySelector('.menu-mobile')
+    if(menumobile.classList.contains('open')){
+        menumobile.classList.remove('open')
+    }else{
+        menumobile.classList.add(open)
+    }
+}
