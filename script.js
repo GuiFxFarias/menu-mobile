62495d3e11e2072205677ba085ec600d71df3f28
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-16 13:26:43 -0300

    finish

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function menuShow(){
-    let menumobile = document.querySelector('.menu-mobile')
-    if(menumobile.classList.contains('open')){
-        menumobile.classList.remove('open')
+    let menumobile = document.querySelector('.menu-mobile');
+    if (menumobile.classList.contains('open')){
+        menumobile.classList.remove('open');
+        document.querySelector('.icon').src = 'menu_white_36dp.svg'
     }else{
-        menumobile.classList.add(open)
+        menumobile.classList.add('open')
+        document.querySelector('.icon').src = 'close_white_36dp.svg'
     }
 }
