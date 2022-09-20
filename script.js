ec4e549aba1440ff710829d65c789e917cbe76ed
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-20 08:02:17 -0300

    nothing

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ function menuShow(){
     let menumobile = document.querySelector('.menu-mobile');
     if (menumobile.classList.contains('open')){
         menumobile.classList.remove('open');
-        document.querySelector('.icon').src = 'menu_white_36dp.svg'
     }else{
-        menumobile.classList.add('open')
-        document.querySelector('.icon').src = 'close_white_36dp.svg'
+        menumobile.classList.add('open');
     }
 }
